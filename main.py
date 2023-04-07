5648e9078ee49d129d61a2fafdf1a894cc149127
~iz <43661631+mencretsu@users.noreply.github.com>
2023-04-07 22:39:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,88 @@
-import os, sys
-import json
+import os
+import sys
 import pyfiglet
-from time import sleep
 from termcolor import colored
+from run import scraping, shilling
 
-f = pyfiglet.Figlet(font="graffiti")
-banner = colored(f.renderText("TWIST"), "green")
-print(banner,colored("Twitter Shilling Bot | Github : @mencretsu\n","grey"))
-usertweet = ""
-while True: 
-    if usertweet == "":
-        print(colored("U're not login yet.","red"))
-    else:print(f"Login as {usertweet}")
-    print("\nMENU:\n1. Login\n2. Run\n3. Help\n4. Update")
-    tinput = input("Input: ")
+ok = False
+username = ""
 
-    if tinput == "1":
-        while True:
-            try:
-                import login
-                if login.SUCCESS == True:
-                    usertweet = "jembot"
-                    break
-                else:
-                    print("Error!")
-                    pass
-            except:pass
-    if tinput == "2":
-        pass
-sleep(1000)
-if tuser == "l" or tuser == "L":
-    while True:
-        prefix = False
-        fake_name = input("enter project name: ")
-        fake_text = input("enter text (max 280):")
-        fake_prefix = input("use prefix? Y/N :")
-        prefix_value = 0
-        if fake_prefix == "y" or fake_prefix == "Y":
-            prefix = True
-            while True:
-                try:
-                    prefix_value = int(input('input prefix:'))
-                    break
-                except ValueError:
-                    print("Invalid input, please enter a valid number :")
-        else:pass
-        print(fake_name,fake_text, prefix, prefix_value)
-        data = {
-            "fake_name": fake_name,
-            "fake_text": fake_text,
-            "use_prefix": prefix,
-            "prefix_value": prefix_value
-        }
-        if os.path.exists("fake_data.json"):
-            with open("fake_data.json", "r") as fle:
-                all_data = json.load(file)
-            all_data["fake"] = data
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+def login_user():
+    try:
+        from login import login
+        tusername, driver = login()
+        if tusername:
+            print(colored("Your twitter is online\n","green"))  
+            ok = True
         else:
-            all_data = [data]
-        with open("fake_data.json", "w") as file:
-            json.dump(all_data, file)
-        fake =  input("data saved! \nback to menu/run? B/R :")
-        if fake == "r" or fake == "R":
-            break
-        else:pass
-else:pass
-fake_run = input("run bot? y/n:\n")
-if fake_run == "y" or fake_run == "Y":
-    import faketweet_twint
-else:pass
+            print("Error!")
+            return
+        driver.quit()
+        return
+    except ImportError:
+        print("Login module not found.")
+        return
+    except:
+        print("An error occurred during login.")
+        return
+    main_menu()
+def run_bot():
+    sc = scraping()
+    sh = shilling()
+    print("Scrapping twitter...")
+def show_help():
+    clear()
+    print("Help menu:")
+    print("1. Login - log in to Twitter")
+    print("2. Run - run the Twitter shilling bot")
+    print("3. Help - show this help menu")
+    print("4. Update - update the bot")
+    print("\nTwitter account : https://t.me/EfaemService")
+    input("\nPress 'ENTER' to continue")
+def update_bot():
+    print("Updating the bot...")
+def main_menu():
+    global ok, username
+    options = {
+        "1": login_user,
+        "2": run_bot,
+        "3": show_help,
+        "4": update_bot,
+    }
+    while True:
+        banner = colored(pyfiglet.Figlet(font="cricket").renderText("TWIST"), "yellow")
+        print(banner, colored("Twitter Shilling Bot | Github : @mencretsu\n", "blue"))
+        if not ok:
+            try:
+                from login import login
+                tusername, driver = login()
+                if tusername:
+                    username = tusername
+                    ok = True
+            except ImportError:
+                print("Login module not found.")
+                tusername = ""
+            except:
+                print("An error occurred during login.....")
+                tusername = ""
+        if not username:
+            tusername = ""
+            print(colored("U're not logged in yet.\n", "red"))
+        else:
+            print(colored("Your twitter is online\n","green"))
+
+        # driver.quit()
+        print("1. Login")
+        print("2. Run")
+        print("3. Help")
+        print("4. Update")
+        print("5. Exit")
+        choice = input("Input: ")
+        if choice in options:
+            options[choice]()
+        else:
+            print("Invalid input. Please try again.")
+if __name__ == '__main__':
+    main_menu()
