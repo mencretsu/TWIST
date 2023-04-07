835f3a59a0625c6d378d3c45256dbae5882d05a2
~iz <43661631+mencretsu@users.noreply.github.com>
2023-04-08 04:36:13 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,76 +1,68 @@
-import chromedriver_autoinstaller
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
+# pip3 install git+https://github.com/JustAnotherArchivist/snscrape.git
+import pandas as pd
+import snscrape.modules.twitter as smt
 import json
 from time import sleep
-chromedriver_autoinstaller.install()
-import getpass
+from termcolor import colored
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException, StaleElementReferenceException, NoSuchElementException, ElementNotInteractableException, WebDriverException
 
-def login():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    # chrome_options.headless = True
-    driver = webdriver.Chrome(options=chrome_options)
-    try:
-        with open('cookies.json', 'r') as f:
-            cookies = json.load(f)
-        driver.get('https://twitter.com')
-        for cookie in cookies:
-            driver.add_cookie(cookie)
-        driver.refresh()
-        wait = WebDriverWait(driver, 20)
-        username_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="AppTabBar_Profile_Link"]')))
-        username = username_element.get_attribute("href").split("/")[-1]
-        driver.implicitly_wait(10)
-        warnings = driver.find_elements(By.XPATH, "//*[contains(text(), 'warning')]")
-        errors = driver.find_elements(By.XPATH, "//*[contains(text(), 'error')]")
-        if warnings or errors:
-            print("There may be a problem with your account.")
-            username = ""
-            driver.quit()
-        
-        tusername = str(username)
-        return tusername, driver
-    except:
-        while True:
-            print("Login to Twitter")
-            tusername = input("Username/mail/phone : ")
-            tpass = getpass.getpass(prompt="Password : ")
-            driver.get("https://twitter.com/login")
-            driver.implicitly_wait(15)
-
-            userinput = driver.find_element(By.NAME, "text")
-            userinput.send_keys(tusername)
-            userinput.send_keys(Keys.ENTER)
+content = ""
+def scraping():
+    global content
+    key = input("Please input any keyword ex: BTC or Elon Musk: ")
+    max = int(input("Input number of tweet ex: 100: "))
+    content = input("Input ur twitter text (max 280 char): ")
+    scraper = smt.TwitterSearchScraper(key)
+    tweets = []
+    for i, tweet in enumerate(scraper.get_items()):
+        data = [tweet.url]
+        tweets.append(data)
+        if i > max:
+            break
+    tweet_df = pd.DataFrame(tweets, columns=["# Twitter url"])
+    tweet_df.to_csv("scrape.csv", index=False)
+    print("scraping done")
+def shilling():
+    global content
+    from login import login
+    tusername, driver = login()
+    total = 0
+    with open('scrape.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        links = list(dict.fromkeys(line[0] for line in reader))
+    for link in links:
+        driver.get(link)
+        sleep(60)
+        try:
+            driver.implicitly_wait(10)
+            reply = driver.find_element(By.CSS_SELECTOR,'[data-testid="reply"]')
+            driver.execute_script("arguments[0].scrollIntoView();", reply)
+            reply.click()
+            driver.implicitly_wait(10)
+            driver.find_element(By.CSS_SELECTOR,'[data-testid="tweetTextarea_0"]').send_keys(str(content))
+            driver.implicitly_wait(10)
+            driver.find_element(By.CSS_SELECTOR,'[data-testid="tweetButton"]').click()
             try:
-                element_present = EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="toast"]'))
-                WebDriverWait(driver, 10).until(element_present)
-                print("Error can't find ur account")
-                continue
+                driver.implicitly_wait(15)
+                driver.find_element(By.XPATH, "//span[normalize-space()='View']").click()
+                myurl = driver.current_url
+                print("- "+str(myurl))
+                total += 1
             except:
-                print("email verified")
-
-            driver.implicitly_wait(15)
-            passinput= driver.find_element(By.NAME, "password")
-            passinput.send_keys(tpass)
-            passinput.send_keys(Keys.ENTER)
-            try:
-                element_present = EC.presence_of_element_located((By.CSS_SELECTOR, '[data-testid="toast"]'))
-                WebDriverWait(driver, 10).until(element_present)
-                print("Error: Invalid password")
-                continue
-            except:
-                print("Login successful")
-                cookies = driver.get_cookies()
+                warnings = driver.find_elements(By.XPATH, "//*[contains(text(), 'warning')]")
+                errors = driver.find_elements(By.XPATH, "//*[contains(text(), 'error')]")
                 if warnings or errors:
-                    print("There may be a problem with your account.")
-                    username = ""
-                    driver.quit()
-                    break
-                with open('cookies.json', 'w') as f:
-                    json.dump(cookies, f)
-                return tusername, driver
-            
+                    print(colored("There may be a problem with your account.","light_red"))
+                    return
+                driver.refresh()
+                pass
+        except(ElementClickInterceptedException, StaleElementReferenceException, WebDriverException, ElementNotInteractableException):
+            pass
+    print(colored(f"\nTotal sent: {total}","light_green"))
+    input("\nPress 'ENTER' to continue")
+    driver.quit()
+if __name__ == '__main__':
+    scraping()
+    shilling()
