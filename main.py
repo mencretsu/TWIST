cb5b511140b635860908ca46ff1df6aa93162ae4
~iz <43661631+mencretsu@users.noreply.github.com>
2023-04-08 04:35:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import sys
 import pyfiglet
 from termcolor import colored
-from bot import scraping, shilling
+import bot as botting
+import subprocess
 
 ok = False
 username = ""
@@ -14,7 +15,7 @@ def login_user():
         from login import login
         tusername, driver = login()
         if tusername:
-            print(colored("Your twitter is online\n","green"))  
+            print(colored(f"~ Login as @{tusername}\n", "light_green"))
             ok = True
         else:
             print("Error!")
@@ -25,13 +26,15 @@ def login_user():
         print("Login module not found.")
         return
     except:
-        print("An error occurred during login.")
+        print(colored("An error occurred during login.","light_red"))
         return
     main_menu()
 def run_bot():
-    sc = scraping()
-    sh = shilling()
-    print("Scrapping twitter...")
+    if not username:
+        print(colored("\nTo run this bot, you need a Twitter account. Please login first!\n", "yellow"))
+        input("\nPress 'ENTER' to go back")
+        main_menu()
+    botting()
 def show_help():
     clear()
     print("Help menu:")
@@ -42,9 +45,11 @@ def show_help():
     print("\nTwitter account : https://t.me/EfaemService")
     input("\nPress 'ENTER' to continue")
 def update_bot():
-    print("Updating the bot...")
+    subprocess.run(["pip", "install", "--upgrade", "git+https://github.com/mencretsu/twist.git"])
+    input("\nPress 'ENTER' to continue")
 def main_menu():
     global ok, username
+    
     options = {
         "1": login_user,
         "2": run_bot,
@@ -52,34 +57,33 @@ def main_menu():
         "4": update_bot,
     }
     while True:
-        banner = colored(pyfiglet.Figlet(font="cricket").renderText("TWIST"), "yellow")
-        print(banner, colored("Twitter Shilling Bot | Github : @mencretsu\n", "blue"))
-        if not ok:
-            try:
-                from login import login
-                tusername, driver = login()
-                if tusername:
-                    username = tusername
-                    ok = True
-            except ImportError:
-                print("Login module not found.")
-                tusername = ""
-            except:
-                print("An error occurred during login.....")
-                tusername = ""
+        clear()
+        banner = colored(pyfiglet.Figlet(font="graffiti").renderText("TWIST"), "cyan")
+        print(banner, colored("Twitter Shilling Bot | Github : @mencretsu\n","white","on_cyan"))
+        # if not ok:
+        #     try:
+        #         from login import login
+        #         tusername, driver = login()
+        #         if tusername:
+        #             username = tusername
+        #             ok = True
+        #     except ImportError:
+        #         print("Login module not found.")
+        #         tusername = ""
+        #     except:
+        #         print("An error occurred during login.....")
+        #         tusername = ""
         if not username:
             tusername = ""
-            print(colored("U're not logged in yet.\n", "red"))
+            print(colored("~ U're not logged in yet.\n", "light_red"))
         else:
-            print(colored("Your twitter is online\n","green"))
-
+            print(colored(f"~ Login as @{tusername}\n", "light_green"))
         # driver.quit()
         print("1. Login")
         print("2. Run")
         print("3. Help")
         print("4. Update")
-        print("5. Exit")
-        choice = input("Input: ")
+        choice = input(colored("Input: ","cyan"))
         if choice in options:
             options[choice]()
         else:
